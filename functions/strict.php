92c627b4e0c481595dcf9ab8b1deff579843a87c
miglekras <55715761+miglekras@users.noreply.github.com>
2019-10-15 22:20:40 +0300

    homework

--- functions/strict.php
+++ functions/strict.php
@@ -1,15 +1,20 @@
 <?php
+
 declare(strict_types=1);
+
 namespace Nfq\Akademija\Strict;
 
 function calculateHomeWorkSum(int ...$numbers): int
 {
     return array_sum($numbers);
 }
-global $result;
-try{
-$result = calculateHomeWorkSum(3, 2.2, '1');
-}
-catch(\Throwable $exp){
-    $result = 'Bad input';
+
+function answer()
+{
+    try {
+        $result = calculateHomeWorkSum(3, 2.2, '1');
+    } catch (\Throwable $exp) {
+        $result = 'Bad input';
+    }
+    return $result;
 }
